6be47e00e55d6af195898e28ffc3a2405571e85b
Nate Richardson <hello@naterichardson.com>
2017-07-26 10:48:02 -0400

    added !ban command, fixed !kick command. progress towards #15

--- commands/moderation.js
+++ commands/moderation.js
@@ -2,7 +2,8 @@ const GReYBot = require('../greybot');
 
 exports.commands = [
 	'prune',
-	'kick'
+	'kick',
+	'ban'
 ]
 
 var lastPruned = new Date().getTime() - (GReYBot.Config.pruneInterval * 1000);
@@ -24,7 +25,7 @@ exports.prune = {
 	process: (msg, suffix) => {
 		var count = parseInt(suffix) || 0;
 
-		if (msg.member.hasPermission('MANAGE_MESSAGES')) {
+		if (msg.member.hasPermission('MANAGE_MESSAGES', true)) {
 			var timeSinceLastPrune = Math.floor(new Date().getTime() - lastPruned);
 			console.log(timeSinceLastPrune);
 			if (timeSinceLastPrune > (GReYBot.Config.pruneInterval * 1000)) {
@@ -51,7 +52,7 @@ exports.prune = {
 				msg.channel.send({
 					embed: {
 						color: GReYBot.Config.defaultEmbedColor,
-						description: `You can't do that yet, please wait ${wait} second${wait > 1 ? "s" : ""}`
+						description: `You can't do that yet, please wait ${wait} second${wait > 1 ? 's' : ''}`
 					}
 				}).then(message => message.delete(5000));
 			}
@@ -68,43 +69,97 @@ exports.prune = {
 }
 
 exports.kick = {
-	usage: '<user> <reason>',
+	usage: '<user> [reason]',
 	description: 'Kick a user with an optional reason. Requires both the command user and the bot to have kick permission',
 	process: (msg, suffix) => {
-		let args = suffix.split(" ");
+		let args = suffix.split(' ');
 		if (args.length > 0 && args[0]) {
-			let hasPermissonToKick =  msg.guild.me.hasPermission("KICK_MEMBERS");
+			let hasPermissonToKick =  msg.guild.me.hasPermission('KICK_MEMBERS', true);
 			if (!hasPermissonToKick) {
-				msg.channel.send( "I don't have permission to kick people!");
+				msg.channel.send( 'I don\'t have permission to kick people!');
 				return;
 			}
-			if (!msg.member.hasPermission("KICK_MEMBERS")) {
-				msg.channel.send( "You don't have permission to kick people!");
+			if (!msg.member.hasPermission('KICK_MEMBERS', true)) {
+				msg.channel.send( 'You don\'t have permission to kick people!');
 				return;
 			}
 			let targetId = resolveMention(args[0]);
 			msg.guild.fetchMember(targetId).then(member => {
 				if (member != undefined) {
 					if (!member.kickable) {
-						msg.channel.send("I can't kick " + member + ". Do they have the same or a higher role than me?");
+						msg.channel.send('I can\'t kick ' + member + '. Do they have the same or a higher role than me?');
 						return;
 					}
 					if (args.length > 1) {
-						let reason = args.slice(1).join(" ");
+						let reason = args.slice(1).join(' ');
 						member.kick(reason).then(x => {
-							msg.channel.send("Kicking " + member + " from " + msg.guild + " for " + reason + "!");
-						}).catch(err => msg.channel.send("Kicking " + member + " failed:\n"));
+							msg.channel.send('Kicking ' + member + ' from ' + msg.guild + ' for ' + reason + '!');
+						}).catch(err => msg.channel.send('Kicking ' + member + ' failed:\n'));
 					} else {
 						member.kick().then(x => {
-							msg.channel.send("Kicking " + member + " from " + msg.guild + "!");
-						}).catch(err => msg.channel.send("Kicking " + member + " failed:\n"));
+							msg.channel.send('Kicking ' + member + ' from ' + msg.guild + '!');
+						}).catch(err => msg.channel.send('Kicking ' + member + ' failed:\n'));
 					}
 				} else {
-					msg.channel.send("I couldn't find a user " + args[0]);
+					msg.channel.send('I couldn\'t find a user ' + args[0]);
 				}
 			});
 		} else {
-			msg.channel.send("You must specify a user to kick!");
+			msg.channel.send('You must specify a user to kick!');
 		}
+	}
+}
+
+exports.ban = {
+	usage: '<user> [days of messages to delete] [reason]',
+	description: 'bans the user, optionally deleting messages from them in the last x days',
+	process: function(msg, suffix){
+		var args = suffix.split(' ');
+		if (args.length > 0 && args[0]) {
+			if (!msg.guild.me.hasPermission('BAN_MEMBERS', true)) {
+				msg.channel.send( 'I don\'t have permission to ban people!');
+				return;
+			}
+			if (!msg.member.hasPermission('BAN_MEMBERS', true)) {
+				msg.channel.send( 'You don\'t have permission to ban people!');
+				return;
+			}
+			let targetId = resolveMention(args[0]);
+			msg.guild.fetchMember(targetId).then(member => {
+				if (member != undefined) {
+					if (!member.bannable) {
+						msg.channel.send('I can\'t ban ' + member + '. Do they have the same or a higher role than me?');
+						return;
+					}
+					if (args.length > 1) {
+						if(typeof args[1] === 'number') {
+							if (args.length > 2) {
+								let days = args[1];
+								let reason = args.slice(2).join(' ');
+								member.ban(reason).then(x => {
+									msg.channel.send('Banning ' + member + ' from ' + msg.guild + ' for ' + reason + '!');
+								}).catch(err => msg.channel.send('Banning ' + member + ' failed:\n'));
+							} else {
+								let days = args[1];
+								member.ban(days).then(x => {
+									msg.channel.send('Banning ' + member + ' from ' + msg.guild + '!');
+								}).catch(err => msg.channel.send('Banning ' + member + ' failed:\n'));
+							}
+						} else {
+							let reason = args.slice(1).join(' ');
+							member.ban(reason).then(x => {
+								msg.channel.send('Banning ' + member + ' from ' + msg.guild + ' for ' + reason + '!');
+							}).catch(err => msg.channel.send('Banning ' + member + ' failed:\n'));
+						}
+					} else {
+						member.ban().then(x => {
+							msg.channel.send('Banning ' + member + ' from ' + msg.guild + '!');
+						}).catch(err => msg.channel.send('Banning ' + member + ' failed:\n'));
+					}
+				}
+			});
+		} else {
+			msg.channel.send('You must specify a user to ban!');
+		};
 	}
 }
