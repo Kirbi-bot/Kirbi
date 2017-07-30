41a24eb5f813a76bbda7b9dc7aa19321f9eaf36f
Nate Richardson <hello@naterichardson.com>
2017-07-30 10:09:18 -0400

    fixed spacing and file naming

--- commands/cocktail-lookup.js
+++ commands/cocktail-lookup.js
@@ -17,7 +17,7 @@ exports.cocktail = {
 			var response = JSON.parse(body);
 			if (typeof response !== 'undefined' && response.drinks !== null) {
 				let result = response.drinks[0];
-        let ingredients = Object.entries(result).slice(7,22).map(entry => entry[1]);
+        		let ingredients = Object.entries(result).slice(7,22).map(entry => entry[1]);
 				let ratios = Object.entries(result).slice(22,37).map(entry => entry[1]);
 
 				if (typeof result.strInstructions !== 'undefined' && result.strInstructions !== '') {
@@ -29,50 +29,50 @@ exports.cocktail = {
 						thumbnail = 'https://emojipedia-us.s3.amazonaws.com/thumbs/240/twitter/103/tropical-drink_1f379.png';
 					}
 					if (typeof result.strGlass !== 'undefined' && result.strGlass !== '') {
-							fields.push({
-									name: 'Glass:',
-									value: result.strGlass,
-									inline: true
-							});
+						fields.push({
+							name: 'Glass:',
+							value: result.strGlass,
+							inline: true
+						});
 					};
 					if (typeof ingredients !== 'undefined' && ingredients.length) {
-							let ingredientsList = '';
-							ingredients.forEach((element, index) => {
-									if (element !== '' && element !== '\n') {
-											if (ratios[index] !== '' && ratios[index] !== '\n') {
-													ingredientsList += `* ${element} - ${ratios[index]}\n`;
-											} else {
-													ingredientsList += `* ${element}\n`;
-											}
-									};
-							});
-							fields.push({
-									name: 'Ingredients:',
-									value: ingredientsList,
-									inline: true
-							});
+						let ingredientsList = '';
+						ingredients.forEach((element, index) => {
+							if (element !== '' && element !== '\n') {
+								if (ratios[index] !== '' && ratios[index] !== '\n') {
+									ingredientsList += `* ${element} - ${ratios[index]}\n`;
+								} else {
+									ingredientsList += `* ${element}\n`;
+								}
+							};
+						});
+						fields.push({
+							name: 'Ingredients:',
+							value: ingredientsList,
+							inline: true
+						});
 					};
 					if (typeof result.strInstructions !== 'undefined' && result.strInstructions !== '') {
-							fields.push({
-									name: 'Instructions:',
-									value: result.strInstructions,
-							});
+						fields.push({
+							name: 'Instructions:',
+							value: result.strInstructions
+						});
 					};
 
 					msg.channel.send({
-							embed: {
-									color: GReYBot.Config.defaultEmbedColor,
-									title: `__${result.strDrink}__`,
-									author: {
-										name: 'CocktailDB',
-										url: 'http://www.thecocktaildb.com/',
-										icon_url: 'https://emojipedia-us.s3.amazonaws.com/thumbs/240/twitter/103/cocktail-glass_1f378.png'
-									},
-									thumbnail: {
-											url: thumbnail
-									},
-									fields: fields
-							}
+						embed: {
+							color: GReYBot.Config.defaultEmbedColor,
+							title: `__${result.strDrink}__`,
+							author: {
+								name: 'CocktailDB',
+								url: 'http://www.thecocktaildb.com/',
+								icon_url: 'https://emojipedia-us.s3.amazonaws.com/thumbs/240/twitter/103/cocktail-glass_1f378.png'
+							},
+							thumbnail: {
+									url: thumbnail
+							},
+							fields: fields
+						}
 					});
 				} else {
 					msg.channel.send({
